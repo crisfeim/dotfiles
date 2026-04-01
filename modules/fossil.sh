30ed2b1c8e1fac0c1defdd0acb4e17d30263ae1f
Cristian Rojas <cristian.rojas@live.fr>
2026-04-01 14:03:40 +0200

    Fix fossil prompt not working when navigating to a subfolder and parent repository inside a git repository

--- modules/fossil.sh
+++ modules/fossil.sh
@@ -1,10 +1,13 @@
 function fossil_prompt_info() {
-	if [[ -f .fslckout || -f _FOSSIL_ ]]; then
-		local branch=$(fossil branch current 2>/dev/null)
-		# %F{62} -> Índigo
-		# %F{209} -> Salmón
-		echo -n "%B%F{105}fossil:(%F{209}${branch}%F{105})%b%f "
-	fi
+    local dir="$PWD"
+    while [[ "$dir" != "/" ]]; do
+        if [[ -f "$dir/.fslckout" || -f "$dir/_FOSSIL_" ]]; then
+            local branch=$(fossil branch current 2>/dev/null)
+            echo -n "%B%F{105}fossil:(%F{209}${branch}%F{105})%b%f "
+            return
+        fi
+        dir="${dir:h}"
+    done
 }
 
 setopt prompt_subst
