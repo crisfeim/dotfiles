81e1dab6d20d6aef468fe2dc24225e2854f5c85b
Cristian Rojas <cristian.rojas@live.fr>
2026-04-20 00:52:34 +0200

    mezclar fossil prompt con módulo vcs

--- modules/vcs.sh
+++ modules/vcs.sh
@@ -345,3 +345,18 @@ fopen() {
     cd "$folder_name"
     fossil open "../$db_file"
 }
+
+function fossil_prompt_info() {
+    local dir="$PWD"
+    while [[ "$dir" != "/" ]]; do
+        if [[ -f "$dir/.fslckout" || -f "$dir/_FOSSIL_" ]]; then
+            local branch=$(fossil branch current 2>/dev/null)
+            echo -n "%B%F{105}fossil:(%F{209}${branch}%F{105})%b%f "
+            return
+        fi
+        dir="${dir:h}"
+    done
+}
+
+setopt prompt_subst
+PROMPT+='$(fossil_prompt_info)'
