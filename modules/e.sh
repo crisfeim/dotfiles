a2d8609129235ff354e7f74e141ccada1746a98c
Cristian Rojas <cristian.rojas@live.fr>
2026-05-19 22:04:56 +0200

    Move e to its own file

---
+++ modules/e.sh
@@ -0,0 +1,19 @@
+# Lazyfica e
+# Editará el primer fichero que haga match con el filename independientemente de la extensión.
+e() {
+  local args=()
+  for arg in "$@"; do
+    if [[ -e "$arg" ]]; then
+      args+=("$arg")
+    else
+      local match
+      match=($(ls "${arg}".* 2>/dev/null | head -1))
+      if [[ -n "$match" ]]; then
+        args+=("$match")
+      else
+        args+=("$arg")
+      fi
+    fi
+  done
+  nvim "${args[@]}"
+}
--- modules/misc.sh
+++ modules/misc.sh
@@ -1,22 +1,3 @@
-# Lazyfica e
-# Editará el primer fichero que haga match con el filename independientemente de la extensión.
-e() {
-  local args=()
-  for arg in "$@"; do
-    if [[ -e "$arg" ]]; then
-      args+=("$arg")
-    else
-      local match
-      match=($(ls "${arg}".* 2>/dev/null | head -1))
-      if [[ -n "$match" ]]; then
-        args+=("$match")
-      else
-        args+=("$arg")
-      fi
-    fi
-  done
-  nvim "${args[@]}"
-}
 
 k() { lsof -ti :$1 | xargs -r kill -9 }
 
