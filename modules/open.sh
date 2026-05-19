888554fd8aa2b4527380b3bda61c85a3cb68a668
Cristian Rojas <cristian.rojas@live.fr>
2026-05-19 21:48:14 +0200

    remove unneeded quotes

--- modules/open.sh
+++ modules/open.sh
@@ -1,5 +1,5 @@
 coderunner() { open -a /Applications/Apps/dev/CodeRunner.app "$@" }
 xcode() { open -a Xcode $1 }
 nova() { open -a "/Applications/Apps/dev/Nova 9.6.app" "$@" }
-iina() { open -a "/Applications/Apps/misc/IINA.app" "$@" }
-zed() { open -a "/Applications/Apps/dev/Zed.app" "$@" }
+iina() { open -a /Applications/Apps/misc/IINA.app "$@" }
+zed() { open -a /Applications/Apps/dev/Zed.app "$@" }
