773ea3804eeefa73a790c0f77b61dc8564c06132
Cristian Rojas <cristian.rojas@live.fr>
2026-05-19 22:08:39 +0200

    lazify vi

--- modules/e.sh
+++ modules/e.sh
@@ -17,3 +17,21 @@ e() {
   done
   nvim "${args[@]}"
 }
+
+vi() {
+  local args=()
+  for arg in "$@"; do
+    if [[ -e "$arg" ]]; then
+      args+=("$arg")
+    else
+      local match
+      match=($(ls "${arg}".* 2>/dev/null | head -1))
+      if [[ -n "$match" ]]; then
+        args+=("$match")
+      else
+        args+=("$arg")
+      fi
+    fi
+  done
+  command vi "${args[@]}"
+}
