633446929516bac957c5b8aa7528376a64c9d1fa
Cristian Rojas <cristian.rojas@live.fr>
2026-06-24 04:15:22 +0200

    coderunner: remove from now strict concurrency flag

--- configs/coderunner/swift.sh
+++ configs/coderunner/swift.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 [ -z "$CR_SUGGESTED_OUTPUT_FILE" ] && CR_SUGGESTED_OUTPUT_FILE="$PWD/${CR_FILENAME%.*}"
 if [ "$CR_FILENAME" = "main.swift" ]; then
-    xcrun -sdk macosx swiftc -strict-concurrency=minimal -o "$CR_SUGGESTED_OUTPUT_FILE" *.swift "${@:1}" ${CR_DEBUGGING:+-g}
+	xcrun -sdk macosx swiftc -o "$CR_SUGGESTED_OUTPUT_FILE" *.swift "${@:1}" ${CR_DEBUGGING:+-g}
 else
-	xcrun -sdk macosx swiftc -strict-concurrency=minimal -o "$CR_SUGGESTED_OUTPUT_FILE" "$CR_FILENAME" "${@:1}" ${CR_DEBUGGING:+-g}
+	xcrun -sdk macosx swiftc -o "$CR_SUGGESTED_OUTPUT_FILE" "$CR_FILENAME" "${@:1}" ${CR_DEBUGGING:+-g}
 fi
 status=$?
 if [ $status -eq 0 ]; then
