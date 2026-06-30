60433aff66d6640689adddd321859c034ffda01c
Cristian Rojas <cristian.rojas@live.fr>
2026-06-30 17:04:19 +0200

    Add short versions

--- modules/tcl.sh
+++ modules/tcl.sh
@@ -1,3 +1,7 @@
 counter() { tclsh ~/dotfiles/modules/tcl/counter.tcl ~/db/.counter "$@"; }
 memo() { tclsh ~/dotfiles/modules/tcl/memo.tcl ~/db/memo "$@"; }
 db() { tclsh ~/dotfiles/modules/tcl/db.tcl ~/db/db.db "$@"; }
+
+c() { counter "$@"; }
+m() { memo "$@"; }
+d() { db "$@"; }
