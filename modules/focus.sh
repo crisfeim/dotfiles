5d001c5184fe54b0373d98672df929378b70968a
Cristian Rojas <cristian.rojas@live.fr>
2026-07-01 15:58:10 +0200

    Add apps to install process

--- modules/focus.sh
+++ modules/focus.sh
@@ -2,6 +2,7 @@ focus() {
     osascript -e 'display notification "Se ha iniciado la sesión de trabajo" with title "Sesión de trabajo"'
     osascript -e 'tell application "System Events" to set quitList to name of every application process whose background only is false and name is not "iTerm2" and name is not "Focus"' -e 'repeat with appName in quitList' -e 'tell application appName to quit' -e 'end repeat'
     open -a "IINA" /Users/cristian/Music/Music/Media.localized/Music/Focus/Youtube/Chimenea.m4a
+    grayscale
 }
 
 chimenea() { iina ~/Music/Music/Media.localized/Music/Focus/Youtube/Chimenea.m4a }
