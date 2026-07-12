dccb4b1b25266d1be0174b4c1021240ef1a68316
Cristian Rojas <cristian.rojas@live.fr>
2026-07-12 14:07:52 +0200

    Improve fossil prompt by coloring branch name depending on tracked / added files

--- modules/prompt.sh
+++ modules/prompt.sh
@@ -4,7 +4,17 @@ function fossil_prompt_info() {
     while [[ "$dir" != "/" ]]; do
         if [[ -f "$dir/.fslckout" || -f "$dir/_FOSSIL_" ]]; then
             local branch=$(fossil branch current 2>/dev/null)
-            echo -n "%B%F{105}fossil:(%F{209}${branch}%F{105})%b%f "
+            local fossil_changes=$(fossil changes 2>/dev/null)
+            local fossil_extras=$(fossil extras 2>/dev/null)
+            local color="209"  # default
+
+            if [[ -n "$fossil_extras" ]] || echo "$fossil_changes" | grep -qE '^(EDITED|DELETED|MISSING|RENAMED|CONFLICT|UPDATED)'; then
+                color="220"  # yellow: there's non tracked/added changes
+            elif echo "$fossil_changes" | grep -qE '^ADDED'; then
+                color="076"  # green: staged changes added
+            fi
+
+            echo -n "%B%F{105}fossil:(%F{${color}}${branch}%F{105})%b%f "
             return
         fi
         dir="${dir:h}"
